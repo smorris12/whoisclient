3a64e57ba9ac76e81dbbb7611a3e6fd395314942
smorris12 <steve007morris@gmail.com>
2014-07-04 13:49:07 -0600

    Improved code for dealing with ip address and ARIN and APNIC databases.

--- whoisclient.py
+++ whoisclient.py
@@ -12,13 +12,16 @@ def telnet_connect(server):
     connection = telnetlib.Telnet(server, 43)
     return connection
 
-def querywhois_server_list(domain):
+def querywhois_server_list(domain, site):
     #parse the whois server list to find the right server for domain
-
     # Let's test if it is a ip address though and if so skip to arin for now
     try:
         is_number = int(domain)
-        return 'whois.arin.net'
+        test_whois = make_whois_connection("whois.arin.net", site + "\n")
+        if "APNIC" in test_whois:
+            return "whois.apnic.net"
+        else:
+            return "whois.arin.net"
     except:
         server_list = open('whois-servers.txt', 'r')
         whois_server = False
@@ -32,14 +35,27 @@ def parse_domain(site):
     top_domain = site[site.rindex(".") + 1:]
     return top_domain
 
-def format_query(query_site):
+def format_query(query_site, server):
     # Determine if commands need to be added to the query
     try:
         is_number = int(query_site[0])
-        output_site = "n " + query_site + "\n"
-        return output_site
+        if server == "whois.arin.net":
+            output_site = "n " + query_site + "\n"
+            return output_site
+        else:
+            return query_site + "\n"
     except:
         return query_site + "\n"
+
+def make_whois_connection(server_name, query):
+    if server_name == "whois.arin.net":
+        query = "n " + query
+
+    whois_connection = telnet_connect(server_name)
+    whois_connection.write(query)
+    output = whois_connection.read_all()
+    whois_connection.close()
+    return output
 
 if __name__ == "__main__":
     # Get 1st argument from the command line
@@ -48,13 +64,10 @@ if __name__ == "__main__":
     except:
         input_site = raw_input('Please enter an address to whois: ')
 
-    who_server = querywhois_server_list(parse_domain(input_site))
+    who_server = querywhois_server_list(parse_domain(input_site), input_site)
 
     # If the domain can't be parsed or their is no whois server
     if who_server == False:
         print 'Domain does not have a valid whois server'
     else:
-        #print querywhois(who_server, format_query(input_site))
-        whois_connection = telnet_connect(who_server)
-        whois_connection.write(format_query(input_site))
-        print whois_connection.read_all()
+        print make_whois_connection(who_server, format_query(input_site, who_server))
